d1e8d978cee4c84c91917ccc5ec1b5810434bbd8
James Kassemi <jkassemi@gmail.com>
2014-04-30 11:47:37 -0600

    Autocomplete and installation for vim

--- vim/install.fish
+++ vim/install.fish
@@ -1,17 +1,34 @@
+brew install vim
+
 cp -R vim/ ~/.vim
 cp vimrc ~/.vimrc
 mkdir -p ~/.vim/bundle
 
-# Believe this is entirely unnecessary, since vundle
-# manages these on 
+##
+
+# Vundle
 if test -e ~/.vim/bundle/vundle
 else
   git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 end
 
-vim +PluginInstall +qall
+# Install vim-go
+# http://blog.gopheracademy.com/vimgo-development-environment
+cd ~/.vim/bundle
 
-if test -e ~/.vim/go.vim
+if test -e vim-go
 else
-  git clone https://github.com/fsouza/go.vim ~/.vim/go.vim
+  git clone https://github.com/fatih/vim-go.git
 end
+
+if test -e YouCompleteMe
+else
+  git clone https://github.com/Valloric/YouCompleteMe.git
+end
+
+cd YouCompleteMe
+git submodule update --init --recursive
+./install.sh
+
+##
+vim +PluginInstall +qall
